bf0f092cb3ff4dd9c4747f89d53bd3a2377ef1b9
Ayormitan <kolawoleabaz97@gmail.com>
2024-02-28 03:33:02 -0800

    Checks for root node

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
+
 /**
- * biary_tree_is_root - Check for root node
+ * binary_tree_is_root - Check for root node
  *
- * @ode: Pointer to te node to check
+ * @node: Pointer to te node to check
+ * Return: Root node on success
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if(node == NULL ||node->parent != NULL)
+	if (node == NULL || node->parent != NULL)
 		return (0);
 	return (1);
 }
